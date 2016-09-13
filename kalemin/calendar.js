f0c60396304aef86d34623bdd2c44b144b323898
Craig Burton <cburton@platformq.com>
2016-09-12 23:54:35 -0400

    Fixed typo

--- kalemin/calendar.js
+++ kalemin/calendar.js
@@ -1,7 +1,7 @@
 (function(){
   'use strict';
 
-  docmuent.on('ready', function(){
+  document.on('ready', function(){
     console.log('snap!');
     console.log(document.getElementById('year-title'));
   });
